b87e422dc0c7c1285501579b0e4e9a085662694e
OFTGNOV <tamai100009@gmail.com>
2025-11-14 22:50:21 -0500

    Extend Driver class from User and add ID management
    Driver now extends User, includes a unique ID field, parameterized and copy constructors, and getter/setter for the ID. Also adds a ToString method to display driver details.

--- src/userModule/Driver.java
+++ src/userModule/Driver.java
@@ -1,5 +1,32 @@
 package userModule;
 
-public class Driver {
+public class Driver extends User {
+	private String id;
+	
+	//Parameterized Constructor
+	public Driver(String Fname,String Lname, String email, String password) {
+		super(Fname, Lname, email, password);
+		this.id = "DRV-" + Math.floor(Math.random() * 10000); // Generating a random ID for Driver
+	}
+	
+	//Copy Constructor
+	public Driver(Driver other) {
+		super(other);
+		this.id = other.id;
+	}
+	
+	public String ToString() {
+		return super.ToString() + 
+				", \nDriver ID: " + id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 
 }
